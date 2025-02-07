c5f22ca420452c9711d59a0297d4e5ca6a8d53fb
aaravbhagyaschool <ab7857@pleasantonusd.net>
2025-02-07 21:58:18 +0000

    commit prev

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -1,66 +1,42 @@
-import java.awt.geom.*; // for Point2D.Double
-import java.util.ArrayList; // for ArrayList
-import java.util.concurrent.TimeUnit;
+import java.awt.geom.*; 
+import java.util.ArrayList;
 
-import gpdraw.*; // for DrawingTool
+import gpdraw.*;
 
+public class IrregularPolygon 
+{
+    private ArrayList<Point2D.Double> myPolygon;
 
-public class IrregularPolygon {
-    private ArrayList<Point2D.Double> myPolygon = new ArrayList<Point2D.Double>();
-
-    // constructor
-    public IrregularPolygon() {}
-
-    public void add(Point2D.Double aPoint) 
+    public IrregularPolygon() 
     {
-        myPolygon.add(aPoint);
+        myPolygon = new ArrayList<>();
     }
-    
+
+    public void add(Point2D.Double point) 
+    {
+        myPolygon.add(point);
+    }
+
     public double perimeter() 
     {
-        if (myPolygon.size() < 2) 
-        {
-            return 0.0;
-        }
+        if (myPolygon.size() < 2) return 0.0;
         double perimeter = 0.0;
-
         for (int i = 0; i < myPolygon.size(); i++) 
         {
-            Point2D.Double current = myPolygon.get(i);
-            Point2D.Double next = myPolygon.get((i + 1) % myPolygon.size());
-            double distance = current.distance(next);
-            perimeter += distance;
+            perimeter += myPolygon.get(i).distance(myPolygon.get((i + 1) % myPolygon.size()));
         }
         return perimeter;
     }
-    
 
-    public double area() {
-        if (myPolygon.size() < 3) {
-            return 0.0;
-        }
-        double sum1 = 0.0;
-        double sum2 = 0.0;
-        for (int i = 0; i < myPolygon.size(); i++) {
-            Point2D.Double current = myPolygon.get(i);
-            Point2D.Double next = myPolygon.get((i + 1) % myPolygon.size());
-            sum1 += current.x * next.y;
-            sum2 += current.y * next.x;
-        }
-        return Math.abs(sum1 - sum2) / 2.0;
-    }
-    
-
-    public void draw()
+//shoelace formula below
+    public double area() 
     {
-        // Wrap the DrawingTool in a try/catch to allow development without need for graphics.
-        try {
-            // TODO: Draw the polygon.
-            // Documents: https://pavao.org/compsci/gpdraw/html/gpdraw/DrawingTool.html
-            DrawingTool pen = new DrawingTool(new SketchPad(500, 500));
-            pen.move(50, 50);
-        } catch (java.awt.HeadlessException e) {
-            System.out.println("Exception: No graphics support available.");
-        }
+     return 0.0;
+    }
+
+    public void draw() 
+    {
+
     }
 }
+
